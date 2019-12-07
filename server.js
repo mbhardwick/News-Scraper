c84416f64377d9d41bd6120d5a62edece5e727a1
MB Hardwick <48660923+mbhardwick@users.noreply.github.com>
2019-12-07 09:07:39 -0800

    server setup

--- server.js
+++ server.js
@@ -25,4 +25,9 @@ app.use(express.static("public"));
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// Start the server
+app.listen(PORT, function () {
+	console.log("App running on port " + PORT);
+});
